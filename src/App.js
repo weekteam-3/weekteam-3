6bb9437bced921744b9640eb015e8d9e472e30b2
Jino0403 <jinho9203@naver.com>
2022-10-14 21:39:15 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Write from "./pages/Write";
+import Main from "./pages/Main";
+import Todo from "./pages/Todo";
+import Edit from "./pages/Edit";
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={< />} />
-      <Route path="/write" element={< />} />
-      <Route path="/:id" element={< />} />
-      <Route path="/edit/:id" element={< />} /> */}
+      <Route path="/" element={<Main />} />
+      <Route path="/write" element={<Write />} />
+      <Route path="/:id" element={<Todo />} />
+      <Route path="/edit/:id" element={<Edit />} />
     </Routes>
   );
 }
