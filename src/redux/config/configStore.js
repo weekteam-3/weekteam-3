6f92185a6bab939b18d153c3261fc92e3fd70843
kimminseok <kms3288381@naver.com>
2022-10-19 21:12:39 +0900

    진짜진짜 최종

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,14 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import card from "../../components/card/Card";
 import todos from "../modules/Modules";
-import cardSlice from "../modules/CardSlice";
+import cardSlice from "../modules/cardSlice";
 import todo from "../modules/todoModule";
-
 
 const store = configureStore({
   reducer: {
     todos: todos.reducer,
-    card: card,
     CardSlice: cardSlice,
     todoModule: todo,
   },
