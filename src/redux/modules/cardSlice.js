cbb661319ec17ae5e9a3ca7d6af754ae62510aca
kimminseok <kms3288381@naver.com>
2022-10-20 19:53:52 +0900

    ...

--- src/redux/modules/cardSlice.js
+++ src/redux/modules/cardSlice.js
@@ -1,16 +1,58 @@
+// import { Action } from "@remix-run/router";
+
+// const ADD_COMMENT = "ADD_COMMENT";
+// const
+
+// export const addComment = (payload) => {
+//     return {
+//         type:ADD_COMMENT,
+//         payload
+//     };
+// };
+
+// const initialState = {
+//     id: "",
+//     name:"",
+//     content:"",
+// };
+
+// const card = (state = initialState, action) => {
+//     switch (Action.type) {
+//         case ADD_COMMENT:
+//             return {
+//                 ...state,
+//                 comment: state.id + state.name + state.content + action.payload,
+//             };
+//         default:
+//             return state;
+//     }
+// };
+
+// export default cardslice;
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 const initialState = {
-  comments: [],
+  comments: [
+    {
+      id: "1",
+      userName: "안녕",
+      userContent: "하세요",
+      isLoading: false,
+      error: null,
+    },
+  ],
 };
 
 export const __addComment = createAsyncThunk(
   "ADD_COMMENT",
   async (payload, thunkAPI) => {
     try {
-      console.log("포스트쪽", payload);
-      const data = await axios.post("http://localhost:3001/comments", payload);
+      const data = await axios.post(
+        "http://localhost:3000/todo/card/:id",
+        payload
+      );
       return thunkAPI.fulfillWithValue(data.data);
     } catch (e) {
       return thunkAPI.rejectWithValue(e);
@@ -22,12 +64,13 @@ export const __getCommentById = createAsyncThunk(
   "GET_COMMENT_BY_ID",
   async (payload, thunkAPI) => {
     try {
-      const data = await axios.get(
-        `http://localhost:3001/comments?ssId=${payload}`
+      const data = await axios.post(
+        "http://localhost:3000/todo/card/:id",
+        payload
       );
       return thunkAPI.fulfillWithValue(data.data);
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e);
     }
   }
 );
@@ -36,10 +79,10 @@ export const __updateComment = createAsyncThunk(
   "UPDATE_COMMENT",
   async (payload, thunkAPI) => {
     try {
-      // `http://localhost:3001/comments/${payload.id}`,payload
-      // 이렇게 적는다는 말은 comments에서 payload.id값을 가진 댓글에 접근해라, 그리고 뒤에 보내준 payload로 수정해라 입니다.
-      const data = await axios.patch(
-        `http://localhost:3001/comments/${payload.id}`,
+      console.log({ payload });
+
+      const data = await axios.post(
+        "http://localhost:3000/todo/card/:id",
         payload
       );
       return thunkAPI.fulfillWithValue(data.data);
@@ -53,10 +96,10 @@ export const __deleteComment = createAsyncThunk(
   "DELETE_COMMENT",
   async (payload, thunkAPI) => {
     try {
-      await axios.delete(`http://localhost:3001/comments/${payload}`);
-      return thunkAPI.fulfillWithValue(payload);
+      const data = await axios.delete("http://localhost:3000/todo/card/:id");
+      return thunkAPI.fulfillWithValue(data.data);
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.code);
+      return thunkAPI.rejectWithValue(error);
     }
   }
 );
@@ -66,7 +109,6 @@ export const cardSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: {
-    // ADD
     [__addComment.pending]: (state) => {
       state.isLoading = true;
     },
@@ -78,42 +120,34 @@ export const cardSlice = createSlice({
       state.isLoading = false;
       state.card = action.payload;
     },
-
-    // GET
+    [__getCommentById.pending]: (state) => {
+      state.isLoading = true;
+    },
     [__getCommentById.fulfilled]: (state, action) => {
       state.isLoading = false;
-      state.comments = action.payload;
+      state.commentById = action.payload;
     },
     [__getCommentById.rejected]: (state, action) => {
       state.isLoading = false;
-      state.comments = action.payload;
+      state.card = action.payload;
     },
-
-    // UPDATE
     [__updateComment.pending]: (state) => {
       state.isLoading = true;
     },
     [__updateComment.fulfilled]: (state, action) => {
-      const target = state.comments.findIndex(
-        (comment) => comment.id === action.payload.id
-      );
-      state.comments.splice(target, 1, action.payload);
+      state.isLoading = false;
+      state.card = action.payload;
     },
     [__updateComment.rejected]: (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
     },
-
-    // DELETE
     [__deleteComment.pending]: (state) => {
       state.isLoading = true;
     },
     [__deleteComment.fulfilled]: (state, action) => {
       state.isLoading = false;
-      const target = state.comments.findIndex(
-        (comment) => comment.id === action.payload
-      );
-      state.comments.splice(target, 1);
+      state.card = action.payload;
     },
     [__deleteComment.rejected]: (state, action) => {
       state.isLoading = false;
@@ -125,3 +159,19 @@ export const cardSlice = createSlice({
 export const { addComment } = cardSlice.actions;
 
 export default cardSlice.reducer;
+
+// export const cardSlice = createSlice({
+//   name: "Card",
+//   initialState,
+
+//   reducers: {
+//     addComment: (state, action) => {
+//       console.log(action);
+//       return {
+//         // ...state,
+//         // comments: [...state.comments, action.payload],
+//         // state.comment = state.id + state.name + state.content + action.payload;
+//       };
+//     },
+//   },
+// });
